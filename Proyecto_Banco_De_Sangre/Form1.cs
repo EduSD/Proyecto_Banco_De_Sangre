8070183e8bd4a71a34da6830ff12aa761a0e6719
Yave-Balderas <id21136@dasc.edu.mx>
2025-02-24 17:48:50 -0600

    Ahí vamos avanzando

--- Proyecto_Banco_De_Sangre/Form1.cs
+++ Proyecto_Banco_De_Sangre/Form1.cs
@@ -12,19 +12,42 @@ namespace Proyecto_Banco_De_Sangre
 {
     public partial class Form1 : Form
     {
+        private int intentos = 0;
+        private const int maxIntentos = 4;
+        private const string codigoCorrecto = "1234"; // No se me ocurrió otra, como no tenemos base de momento así csm xd
+
         public Form1()
         {
             InitializeComponent();
+            btnentrar.Enabled = false;
+            txtcode.TextChanged += txtcode_TextChanged;
+            btnentrar.Click += btnentrar_Click;
         }
 
         private void txtcode_TextChanged(object sender, EventArgs e)
         {
-
+            btnentrar.Enabled = !string.IsNullOrWhiteSpace(txtcode.Text);
         }
 
         private void btnentrar_Click(object sender, EventArgs e)
         {
-
+            if (txtcode.Text == codigoCorrecto)
+            {
+                MessageBox.Show("¡Bienvenido estimado usuario!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                intentos++;
+                if (intentos >= maxIntentos)
+                {
+                    MessageBox.Show("Has excedido el número de intentos. Ni modo xD", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show($"Código incorrecto. Te quedan {maxIntentos - intentos} intentos. Sino recuerda su código vaya a RH por su carta de renuncia", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
         }
     }
 }
